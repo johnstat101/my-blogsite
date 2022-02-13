d0ab6b3ab30f822d036c24b3efac644e217aabf5
Rea <rmrea@kenya-airways.com>
2022-02-13 16:05:49 +0300

    add 404Page

--- app/__init__.py
+++ app/__init__.py
@@ -19,7 +19,9 @@ def create_app(config_name):
 
     # register blueprint
     from .main import main as main_blueprint
+    from . auth import auth as auth_blueprint
 
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint)
 
     return app
