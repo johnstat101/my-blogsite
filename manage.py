2e08fabbcae1ab4fa29fd81756106d55679ebbb8
Rea <rmrea@kenya-airways.com>
2022-02-14 22:31:03 +0300

    edit manage.py file

--- manage.py
+++ manage.py
@@ -28,7 +28,7 @@ def test():
 
 @manager.shell
 def make_shell_context():
-    return dict(app = app,db = db,User = User, Blog = Blog, Comment = Comment)
+    return dict(app = app,db = db,User = User, Blog = Blog, Comment = Comment, Subscriber = Subscriber)
 
 if __name__ == '__main__':
     manager.run()
